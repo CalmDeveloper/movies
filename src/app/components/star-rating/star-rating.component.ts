c6e44d4798d3efda90dc882da480e15b9cc7e9e2
CalmDeveloper <taras.maksymiv.box@gmail.com>
2024-06-30 15:30:25 +0300

    Added mobile version bugs fix

--- src/app/components/star-rating/star-rating.component.ts
+++ src/app/components/star-rating/star-rating.component.ts
@@ -6,7 +6,7 @@ import {Component, Input} from '@angular/core';
   styleUrl: './star-rating.component.css'
 })
 export class StarRatingComponent {
-  @Input()
-  voteAverage: number;
+  @Input() voteAverage: number;
+  @Input() isLarge: boolean = false;
   stars: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 }
